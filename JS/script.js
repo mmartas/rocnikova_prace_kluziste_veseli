07f74aa30e49aca10312df19926c76eab3687383
Martin Šupler <martin.supler@seznam.cz>
2025-11-16 09:55:54 +0100

    česání

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,12 @@ let hamMenuIcon = document.querySelector(".hamMenuIcon")
 let mainNavMenu = document.querySelector(".mainNavMenu")
 
 hamMenuIcon.addEventListener("click", function(event){
-    mainNavMenu.classList.toggle("display_none_flex")
-
     hamMenuIcon.classList.toggle("active");
+    mainNavMenu.classList.toggle("active");
+
+    if(hamMenuIcon.classList.contains("active")) {
+        mainNavMenu.classList.add("box_glass_effect")
+    } else {
+        mainNavMenu.classList.remove("box_glass_effect")
+    }
 })
