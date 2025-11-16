1421bc7f065f623a5aad35ffa89629f23c102595
Martin Šupler <martin.supler@seznam.cz>
2025-11-16 20:36:40 +0100

    česání headeru

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 let hamMenuIcon = document.querySelector(".hamMenuIcon")
 let mainNavMenu = document.querySelector(".mainNavMenu")
+let widthValue = window.matchMedia("(min-width: 930px)")
 
 hamMenuIcon.addEventListener("click", function(event){
     hamMenuIcon.classList.toggle("active");
@@ -9,5 +10,12 @@ hamMenuIcon.addEventListener("click", function(event){
         mainNavMenu.classList.add("box_glass_effect")
     } else {
         mainNavMenu.classList.remove("box_glass_effect")
+    }
+})
+
+widthValue.addEventListener("change", function(event){
+    if(widthValue.matches){
+        hamMenuIcon.classList.remove("active")
+        mainNavMenu.classList.remove("active", "box_glass_effect")
     }
 })
