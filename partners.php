41ab7207f78731c982de34dc49a2f8fcf1765490
Martin Šupler <martin.supler@seznam.cz>
2026-04-24 08:34:26 +0200

    partneri

--- partners.php
+++ partners.php
@@ -44,16 +44,259 @@
             
             <section class="main_partners one_container">
                 <h1 class="title text_color_gradient">partneři</h1>
-                <section class="partners one_section">
-                    <a href="https://www.prostefitko.cz/uvod" target="_blank">
-                        <div class="text_section box_glass_effect">
-                            <div class="wrapper">
-                                <img src="images/prostefitko.jpg" alt="">
-                                <h2 class="title">Prostěfitko</h2>
+                <div class="partners_container one_section">
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/prostefitko.jpg" alt="">
+                                    <h2 class="title">Prostěfitko</h2>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </section>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/agg.jpg" alt="">
+                                    <h2 class="title">Agg</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/benezelenina.jpg" alt="">
+                                    <h2 class="title">Bene zelenina</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/biogena.jpg" alt="">
+                                    <h2 class="title">Biogena</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/dogindock1.jpg" alt="">
+                                    <h2 class="title">Dockindog</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/dogindogbistro.jpg" alt="">
+                                    <h2 class="title">Dogindog bistro</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/elektrohyl.jpg" alt="">
+                                    <h2 class="title">Elektro hýl</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/elektrosedlar.jpg" alt="">
+                                    <h2 class="title">Elektro Sedlář</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/fafek.jpg" alt="">
+                                    <h2 class="title">Fafek</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/florstyle.jpg" alt="">
+                                    <h2 class="title">Florstyle</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/inteza.jpg" alt="">
+                                    <h2 class="title">Inteza</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/koupelnypopelka.jpg" alt="">
+                                    <h2 class="title">Koupelny Popelka</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/Kovokon.jpg" alt="">
+                                    <h2 class="title">Kovokon</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/makuluku.jpg" alt="">
+                                    <h2 class="title">Makuluku</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/nakoledetem.jpg" alt="">
+                                    <h2 class="title">Na kole dětem</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/penzionhermina.jpg" alt="">
+                                    <h2 class="title">Penzion Hermina</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/pneuplus.jpg" alt="">
+                                    <h2 class="title">Pneuplus</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/radiojih.jpg" alt="">
+                                    <h2 class="title">Rádio Jih</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/stavbyremina.jpg" alt="">
+                                    <h2 class="title">Stavby Remina</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/stavbyvanto.jpg" alt="">
+                                    <h2 class="title">Stavby Vanto</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/trigema.jpg" alt="">
+                                    <h2 class="title">Trigema</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/vesbyt.jpg" alt="">
+                                    <h2 class="title">Vesbyt</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/veselinadmoravou.jpg" alt="">
+                                    <h2 class="title">Veselí nad Moravou</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/vesnet.jpg" alt="">
+                                    <h2 class="title">Vesnet</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                    <section class="partners">
+                        <a href="https://www.prostefitko.cz/uvod" target="_blank">
+                            <div class="text_section box_glass_effect">
+                                <div class="wrapper">
+                                    <img src="images/partners/zlomek.jpg" alt="">
+                                    <h2 class="title">Zlomek</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </section>
+                </div>
+                
             </section>
             
             
