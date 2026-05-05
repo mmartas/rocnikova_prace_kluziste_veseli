543b38bff3c378b728bf407f1f6eda03194eaa36
Martin Šupler <martin.supler@seznam.cz>
2026-05-05 07:33:07 +0200

    pripojeni_databaze

--- assets/db.php
+++ assets/db.php
@@ -1,11 +1,17 @@
 <?php
 // připojení do databáze
 function getDb() {
-    $conn = new mysqli("localhost", "root", "", "kalendar");
+    $servername = "sql209.infinityfree.com";
+    $username = "if0_41823991";
+    $password = "suple2496";
+    $dbname = "if0_41823991_kalendar";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Chyba připojení k DB: " . $conn->connect_error);
     }
 
+    echo("vse je ok");
     return $conn;
 }
