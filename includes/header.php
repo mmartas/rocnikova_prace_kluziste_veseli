8f911a435a38a768fd6accc67277a3e74ac98840
Martin Šupler <martin.supler@seznam.cz>
2026-05-11 17:48:08 +0200

    .

--- includes/header.php
+++ includes/header.php
@@ -16,10 +16,10 @@
         </div>
         <nav class="mainNavMenu">
             <ul>
-                <li><a href="pages/price_list.php" class="<?= $currentPage == 'pages/price_list.php' ? 'active' : '' ?> link_white">ceník</a></li>
-                <li><a href="pages/webkamera.php" class="<?= $currentPage == 'pages/webkamera.php' ? 'active' : '' ?> link_white">webkamera</a></li>
-                <li><a href="pages/operating_hours.php" class="<?= $currentPage == 'pages/operating_hours.php' ? 'active' : '' ?> link_white">rozpis ledu/provozní doba</a></li>
-                <li><a href="pages/rezervation.php" class="<?= $currentPage == 'pages/rezervation.php' ? 'active' : '' ?> link_white box_glass_effect_red">rezervace</a></li>
+                <li><a href="pages/price_list.php" class="<?= $currentPage == 'price_list.php' ? 'active' : '' ?> link_white">ceník</a></li>
+                <li><a href="pages/webkamera.php" class="<?= $currentPage == '/webkamera.php' ? 'active' : '' ?> link_white">webkamera</a></li>
+                <li><a href="pages/operating_hours.php" class="<?= $currentPage == '/operating_hours.php' ? 'active' : '' ?> link_white">rozpis ledu/provozní doba</a></li>
+                <li><a href="pages/rezervation.php" class="<?= $currentPage == '/rezervation.php' ? 'active' : '' ?> link_white box_glass_effect_red">rezervace</a></li>
             </ul>
         </nav>
     </div>
